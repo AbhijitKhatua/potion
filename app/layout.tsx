0c57b98d39d1119760661669c2511a3db268c152
AbhijitKhatua <a.k.piintu@gmail.com>
2023-11-15 23:59:07 +0530

    added dark mode theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
   title: 'Potion',
   description: 'Brew your own stuff',
   icons: {
-    icon : [
+    icon: [
       {
         media: "(prefers-color-scheme: dark)",
         url: "/light-logo.svg",
@@ -29,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-key"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
