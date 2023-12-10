7eed361bd962f4dfba3c4695eea61981b5c5e17c
AbhijitKhatua <a.k.piintu@gmail.com>
2023-12-10 23:55:54 +0530

    minor change

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Id } from "@/convex/_generated/dataModel";
 import { LucideIcon } from "lucide-react";
 
 interface ItemProps {
+  id? : Id
   label: string;
   onClick: () => void;
   icon: LucideIcon;
@@ -13,6 +15,7 @@ export const Item = ({ label, onClick, icon: Icon }: ItemProps) => {
     <div
       onClick={onClick}
       role="button"
+      style={{paddingLeft: "12px" }}
       className="group min-h-[27px] text-sm py-1 pl-[12px] pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
     >
       <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
