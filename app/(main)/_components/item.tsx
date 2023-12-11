dd1021376b44755a1e5852e2d6567d317a5ccce6
AbhijitKhatua <a.k.piintu@gmail.com>
2023-12-11 23:59:07 +0530

    added optional types

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,21 +1,42 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
-import { LucideIcon } from "lucide-react";
+import { ChevronDown, ChevronLeft, ChevronRight, LucideIcon } from "lucide-react";
 
 interface ItemProps {
-  id? : Id
+  id? : Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearched?: boolean;
+  level?: boolean;
+  onExpand?: ()=> void;
   label: string;
   onClick: () => void;
   icon: LucideIcon;
 }
 
-export const Item = ({ label, onClick, icon: Icon }: ItemProps) => {
+export const Item = ({ 
+  id, 
+  label, 
+  onClick, 
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearched,
+  level = 0,
+  onExpand,
+  expanded,
+ }: ItemProps) => {
+  const ChevronIcon = expanded? ChevronDown : ChevronRight
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{paddingLeft: "12px" }}
+      style={{
+        paddingLeft: level? `${(level*12) + 12}px` :"12px" 
+      }}
       className="group min-h-[27px] text-sm py-1 pl-[12px] pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
     >
       <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
