83b73557361dc74dfb7d3434884ecd343e389eb3
AbhijitKhatua <a.k.piintu@gmail.com>
2023-12-15 00:08:49 +0530

    Document-list edited

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,9 +1,64 @@
 "use client";
 
-export const DocumntList = () => {
+import { api } from "@/convex/_generated/api";
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { Item } from "./item";
+import { cn } from "@/lib/utils";
+
+interface DocumentListProps {
+    parentDocumentId? : Id<"documents">;
+    level?: number;
+    data?: Doc<"documents">[];
+}
+
+export const DocumntList = ({
+    parentDocumentId,
+    level = 0,
+}: DocumentListProps) => {
+
+    const params = useParams();
+    const router = useRouter();
+
+    const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+    const onExpand = (documentId: string) => {
+       setExpanded(prevExpanded => ({
+        ...prevExpanded,
+        [documentId]: !prevExpanded[documentId]
+       }));
+    };
+
+    const documents = useQuery(api.documents.getSidebar, {
+        parentDocument: parentDocumentId,
+    });
+
+    const onRedirect = (documentId: string) => {
+        router.push(`/documents/${documentId}`);
+    };
+
+    if (documents === undefined) {
+        return <>
+        <Item.Skeleton level={level} />
+        { level === 0 && (
+            <>
+            <Item.Skeleton level={level} />
+            <Item.Skeleton level={level} />
+            </>
+        )};
+        </>
+    };
+
     return (
-        <div>
-            doc
-        </div>
+        <>
+           <p
+           style={{
+            paddingLeft: level? `${(level*12)+ 25}px` : undefined
+           }}
+           className={cn("hidden text-sm font-medium text-muted-foreground/80", expanded && "last:block", level = 0 && "hidden")}
+           >No pages inside</p>
+        </>
     )
 }
