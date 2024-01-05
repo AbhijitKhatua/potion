b10c46a4efbc795f7d52279287595f80035d9cb7
AbhijitKhatua <a.k.piintu@gmail.com>
2024-01-06 01:21:42 +0530

    added search functionality implimented in navigation and through shortcut

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,8 +25,10 @@ import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 export const Navigation = () => {
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -143,7 +145,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItems />
-          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
           <Item label="Settings" icon={Settings} onClick={() => {}} />
           <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
