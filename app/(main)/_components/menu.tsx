09dcca9b5c8c5ad52157bea98c020595379d85a3
AbhijitKhatua <a.k.piintu@gmail.com>
2024-02-18 00:43:54 +0530

    v-0.8

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { MoreHorizontal, Trash } from "lucide-react";
 
 import { Id } from "@/convex/_generated/dataModel";
 import { 
@@ -13,6 +15,8 @@ import {
   DropdownMenuContent,
   DropdownMenuSeparator 
 } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 
@@ -23,8 +27,45 @@ interface MenuProps {
 export const Menu = ({documentId}: MenuProps) => {
 
   const router = useRouter();
-  const user = useUser();
+  const {user} = useUser();
   const archive = useMutation(api.documents.archive)
 
-  return <div>Menu</div>;
+  const onArchive = () => {
+    const promise = archive ({id: documentId})
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      error: "Failed to archive.",
+      success:"Note moved to trash!"
+    });
+
+    router.push("/documents")
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-60" align="end" alignOffset={8} forceMount>
+        <DropdownMenuItem onClick={onArchive}>
+          <Trash className="h-4 w-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator>
+          <div className="text-xs text-muted-foreground p-2">
+            Last edited by : {user?.fullName}
+          </div>
+        </DropdownMenuSeparator>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
+
+Menu.Skeleton = function MenuSkeleton () {
+  return (
+    <Skeleton className="h-10 w-10" />
+  )
+}
