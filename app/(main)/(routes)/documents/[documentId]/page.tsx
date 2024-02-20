8f28b4a748a8e982793f24739d54816ca6e1a373
AbhijitKhatua <a.k.piintu@gmail.com>
2024-02-20 08:30:27 +0530

    Added toobar icon-picker

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -18,25 +18,26 @@ const DocumentIdPage = ({
         documentId: params.documentId
     });
 
-    if(document === undefined) {
-        return (<div>
-            Loading...
-        </div>)
-    }
-
-    if (document === null ) {
+    if (document === undefined) {
         return (
-            <div>Not found</div>
+        <div>
+            Loading...
+        </div>
         )
     }
 
-    return ( 
+    if (document === null) {
+        return <div>Not found</div>
+    }
+
+    return (
         <div className="pb-40">
+            <div className="h-[35vh]" />
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default DocumentIdPage;
