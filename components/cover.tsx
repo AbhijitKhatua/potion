2d584e14db4a22b0b46b9706a8731767158b6aa6
AbhijitKhatua <a.k.piintu@gmail.com>
2024-03-06 00:18:17 +0530

    remove and update covers now synced with db remove and update file

--- components/cover.tsx
+++ components/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
     url?: string;
@@ -19,15 +20,22 @@ export const Cover = ({
     url,
     preview,
 }: CoverImageProps) => {
+    const {edgestore} = useEdgeStore();
     const params = useParams();
     const coverImage = useCoverImage();
     const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-    const onRemove = () => {
+    const onRemove = async () => {
+        if (url) {
+          await edgestore.publicFiles.delete({
+            url: url
+          })
+        }
         removeCoverImage({
-            id: params.documentId as Id<"documents">
-        })
-    }
+          id: params.documentId as Id<"documents">
+        });
+      };
+    
 
     return (
         <div className={cn("relative w-full h-[35vh] group", !url && "h-[12vh]", url && "bg-muted")}>
@@ -36,7 +44,7 @@ export const Cover = ({
             )}
             {url && !preview && (
                 <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
-                    <Button onClick={coverImage.onOpen} className="text-muted-foreground text-xs" variant="outline" size="sm"><ImageIcon className="h-4 w-4 mr-2"/>Change Cover</Button>
+                    <Button onClick={()=>coverImage.onReplace(url)} className="text-muted-foreground text-xs" variant="outline" size="sm"><ImageIcon className="h-4 w-4 mr-2"/>Change Cover</Button>
                     <Button onClick={onRemove} className="text-muted-foreground text-xs" variant="outline" size="sm"><X className="h-4 w-4 mr-2"/>Remove</Button>
                 </div>
             )}
