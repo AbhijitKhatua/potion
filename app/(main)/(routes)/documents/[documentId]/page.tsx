64df0e4c3945a1fb6a58707d95d92d837491afb5
AbhijitKhatua <a.k.piintu@gmail.com>
2024-03-11 22:47:38 +0530

    saving doc data in local machine

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Editor } from "@/components/editor";
+
 
 interface DocumentIdPageProps {
     params: {
@@ -19,6 +21,15 @@ const DocumentIdPage = ({
     const document = useQuery(api.documents.getById, {
         documentId: params.documentId
     });
+
+    const update = useMutation(api.documents.update);
+
+    const onChange = (content: string) => {
+        update({
+            id: params.documentId,
+            content
+        })
+    }
 
     if (document === undefined) {
         return (
@@ -45,6 +56,10 @@ const DocumentIdPage = ({
             <Cover url={document.coverImage} />
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
+                <Editor
+                onChange={onChange}
+                initialContent = {document.content}
+                />
             </div>
         </div>
     );
